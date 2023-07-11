8a5fe173767d07098ef07d0adfb86353e48b868b
JoannaChang <joanna.changc@gmail.com>
2023-07-11 13:55:51 +0100

    create data for fig1

---
+++ simulation/fig_simulations/fig1_sims.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export seed_str='1000000 1000001 1000002 1000003 1000004 1000005 1000006 1000007 1000008 1000009'
+
+# MAKE DATASETS
+python3 tasks/synth_dataset.py
+seeds=($seed_str)
+for seed in ${seeds[@]}
+do
+    python3 tasks/rotate_dataset.py $seed
+done
+
